df7348adfebf2f4639e5e6cac84601e1c15d4282
Jeremie Tourin <trj4985a@ensfsi.onmicrosoft.com>
2026-04-02 16:25:20 +0200

    feat : Fin TP2

--- src/frontiere/BoundaryAcheterProduit.java
+++ src/frontiere/BoundaryAcheterProduit.java
@@ -16,16 +16,51 @@ public class BoundaryAcheterProduit {
 			System.out.println("Je suis désolée " + nomAcheteur +" mais il faut être un habitant de notre village pour commercer ici.");
 		}
 		else {
+			String produit = Clavier.entrerChaine("Que veux tu acheter ?");
+			
 			Gaulois[] vendeurs = controlAcheterProduit.vendeurProduit(produit);
 			if (vendeurs != null) {
 				System.out.println("Désolé, personne ne vend ce produit au marché.");
 			}
 			else {
-				System.out.println("Chez quel commerçant voulez-vous acheter des fleurs ?");
-				for (int i =0; i < vendeurs.length; i++) {
-					
+				System.out.println("Chez quel vendeur voulez vous acheter des " + produit);
+				for (int i = 0; i < vendeurs.length; i++) {
+					System.out.println((i + 1) + " - " + vendeurs[i].getNom());
+				}
+				int noEtal = -1;
+				do {
+					noEtal = Clavier.entrerEntier("");
+					if (noEtal > vendeurs.length) {
+						System.out.println("ce vendeur n'existe pas");
+					}
+				} while (noEtal > vendeurs.length);
+
+				Gaulois vendeur = vendeurs[noEtal - 1];
+
+				System.out.println(nomAcheteur + " se déplace jusqu'a l'etal de " + vendeur.getNom());
+
+				int qte = -1;
+				do {
+					qte = Clavier.entrerEntier("Combien de " + produit + " voulez vous acheter ?");
+					if (qte <= 0) {
+						System.out.println("qte strictement postitive");
+					}
+				} while (qte <= 0);
+
+				int nbProduits = controlAcheterProduit.acheterProduit(vendeur.getNom(), qte);
+				if (nbProduits == 0) {
+					System.out.println(nomAcheteur + " veut acheter " + qte + " " + produit
+							+ ", malheureusement il n'y en a plus !");
+				} else if (nbProduits < qte) {
+					System.out.println(nomAcheteur + " veut acheter " + qte + " " + produit + ", malheureusement "
+							+ vendeur.getNom() + " n'en a plus que " + nbProduits + ".");
+					System.out.println(nomAcheteur + " achète tout le stock de " + vendeur.getNom() + ".");
+				} else {
+					System.out.println(
+							nomAcheteur + " achète " + nbProduits + " " + produit + " à " + vendeur.getNom() + ".");
 				}
 			}
 		}
 	}
+	
 }
